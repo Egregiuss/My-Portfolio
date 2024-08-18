22a0167755739e1070b2908a4c52dfa4d1588700
Egregiuss <daegregius@gmail.com>
2024-08-18 14:43:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,29 +4,7 @@ import Brand from "./images/BRAND.png";
 
 const url = "https://google.com";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [portfolio, setPortfolio] = useState([]);
-
-  const fetchPortfolio = async () => {
-    const reponse = await fetch(url);
-    const newWeb = await reponse.json();
-    setPortfolio(newWeb);
-    setLoading();
-  };
-  useEffect(() => {
-    fetchPortfolio();
-  }, []);
-  if (loading) {
-    return (
-      <section className="loader">
-        <div className="  pt-5 d-flex justify-content-center ">
-          {" "}
-          <img src={Brand} alt="loading" />
-        </div>
-      </section>
-    );
-  }
-
+ 
   return (
     <>
       <Home />
