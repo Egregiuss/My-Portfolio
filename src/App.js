4002e5f866002fbb096e366ad3e9647cf1d77197
Egregiuss <daegregius@gmail.com>
2024-08-18 15:01:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Home } from "./Home";
 import Brand from "./images/BRAND.png";
 
-const url = "https://google.com";
+const url = "https://jsonplaceholder.typicode.com/posts";
 function App() {
 
   const [loading, setLoading] = useState(true);
